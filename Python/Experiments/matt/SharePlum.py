046b93f54e78e6036531201d12958217b42b8fad
Matthew Buchan <mbuchan7@asu.edu>
2020-08-06 00:07:01 -0700

    Website updates and SharePlum updates

--- Python/Experiments/matt/SharePlum.py
+++ Python/Experiments/matt/SharePlum.py
@@ -1,14 +1,14 @@
-# pip install O365
-from O365 import Account
+# # pip install O365
+# from O365 import Account
 
-credentials = ('client_id', 'client_secret')
+# credentials = ('client_id', 'client_secret')
 
-account = Account(credentials)
-m = account.new_message()
-m.to.add('to_example@example.com')
-m.subject = 'Testing!'
-m.body = "George Best quote: I've stopped drinking, but only while I'm asleep."
-m.send()
+# account = Account(credentials)
+# m = account.new_message()
+# m.to.add('to_example@example.com')
+# m.subject = 'Testing!'
+# m.body = "George Best quote: I've stopped drinking, but only while I'm asleep."
+# m.send()
 
 
 # # python -m pip install --upgrade pip
@@ -33,3 +33,47 @@ m.send()
 
 # new_list = site.List('My New List')
 # sp_data = new_list.GetListItems()
+
+
+# # Other
+
+# from office365.runtime.auth.authentication_context import AuthenticationContext
+# from office365.sharepoint.client_context import ClientContext
+
+# url= "https://login.microsoftonline.com/login.srf"
+# ctx_auth = AuthenticationContext(url)
+# if ctx_auth.acquire_token_for_user('mbuchan7@sundevils.asu.edu', 'Fh232tAK%2'):
+#   ctx = ClientContext(url, ctx_auth)
+#   web = ctx.web
+#   ctx.load(web)
+#   ctx.execute_query()
+#   print ("Web title: {0}".format(web.properties['Title']))
+
+# else:
+#     print("NOPE")
+#     print (ctx_auth.get_last_error())
+
+
+
+import json
+
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.runtime.client_request import ClientRequest
+from office365.runtime.utilities.request_options import RequestOptions
+
+tenant_url= "https://login.microsoftonline.com/login.srf"
+ctx_auth = AuthenticationContext(tenant_url)
+
+site_url="https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom/"
+
+if ctx_auth.acquire_token_for_user("mbuchan7@sundevils.asu.edu","Fh232tAK%2"):
+  request = ClientRequest(ctx_auth)
+  options = RequestOptions("{0}/_api/web/".format(site_url))
+  options.set_header('Accept', 'application/json')
+  options.set_header('Content-Type', 'application/json')
+  data = request.execute_request_direct(options)
+  s = json.loads(data.content)
+  web_title = s['Title']
+  print("Web title: " + web_title)
+else:
+  print(ctx_auth.get_last_error())
