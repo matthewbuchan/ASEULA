ff35f2d3c96c27d4ebfc4031d5fad16cc96e1846
Matthew Buchan <mbuchan7@asu.edu>
2020-08-11 22:31:48 -0700

    removed sensitive data

--- Python/Experiments/matt/SharePlum.py
+++ Python/Experiments/matt/SharePlum.py
@@ -13,26 +13,25 @@
 
 # # python -m pip install --upgrade pip
 # # pip install shareplum
-# from shareplum import Site
-# from shareplum import Office365
-# from shareplum.site import Version
-# from requests_ntlm import HttpNtlmAuth
+from shareplum import Site
+from shareplum import Office365
+from shareplum.site import Version
+from requests_ntlm import HttpNtlmAuth
 
 # # Office 365
 
-# auth = Office365('https://arizonastateu.sharepoint.com/', username='mbuchan7@sundevils.asu.edu', password='Fh232tAK%2').GetCookies()
+# auth = Office365('https://arizonastateu.sharepoint.com/adfs/', username='mbuchan7@sundevils.asu.edu', password='').GetCookies()
 # site = Site('https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom/', authcookie=auth)
 # sp_list = site.List('list name')
 # data = sp_list.GetListItems('All Items', rowlimit=200)
 
+# REST API
 
-# # REST API
+auth = Office365('https://arizonastateu.sharepoint.com/adfs', username='mbuchan7@sundevils.asu.edu', password='').GetCookies()
+site = Site('https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom/', version=Version.v365 , authcookie=auth)
 
-# auth = Office365('https://arizonastateu.sharepoint.com', username='mbuchan7@sundevils.asu.edu', password='Fh232tAK%2').GetCookies()
-# site = Site('https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom/', version=Version.v365 , authcookie=auth)
-
-# new_list = site.List('My New List')
-# sp_data = new_list.GetListItems()
+new_list = site.List('My New List')
+sp_data = new_list.GetListItems()
 
 
 # # Other
@@ -42,7 +41,7 @@
 
 # url= "https://login.microsoftonline.com/login.srf"
 # ctx_auth = AuthenticationContext(url)
-# if ctx_auth.acquire_token_for_user('mbuchan7@sundevils.asu.edu', 'Fh232tAK%2'):
+# if ctx_auth.acquire_token_for_user('mbuchan7@sundevils.asu.edu', ''):
 #   ctx = ClientContext(url, ctx_auth)
 #   web = ctx.web
 #   ctx.load(web)
@@ -55,25 +54,25 @@
 
 
 
-import json
+# import json
 
-from office365.runtime.auth.authentication_context import AuthenticationContext
-from office365.runtime.client_request import ClientRequest
-from office365.runtime.utilities.request_options import RequestOptions
+# from office365.runtime.auth.authentication_context import AuthenticationContext
+# from office365.runtime.client_request import ClientRequest
+# from office365.runtime.utilities.request_options import RequestOptions
 
-tenant_url= "https://login.microsoftonline.com/login.srf"
-ctx_auth = AuthenticationContext(tenant_url)
+# tenant_url= "https://login.microsoftonline.com/login.srf"
+# ctx_auth = AuthenticationContext(tenant_url)
 
-site_url="https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom/"
+# site_url="https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom/"
 
-if ctx_auth.acquire_token_for_user("mbuchan7@sundevils.asu.edu","Fh232tAK%2"):
-  request = ClientRequest(ctx_auth)
-  options = RequestOptions("{0}/_api/web/".format(site_url))
-  options.set_header('Accept', 'application/json')
-  options.set_header('Content-Type', 'application/json')
-  data = request.execute_request_direct(options)
-  s = json.loads(data.content)
-  web_title = s['Title']
-  print("Web title: " + web_title)
-else:
-  print(ctx_auth.get_last_error())
+# if ctx_auth.acquire_token_for_user("mbuchan7@sundevils.asu.edu","Fh232tAK%2"):
+#   request = ClientRequest(ctx_auth)
+#   options = RequestOptions("{0}/_api/web/".format(site_url))
+#   options.set_header('Accept', 'application/json')
+#   options.set_header('Content-Type', 'application/json')
+#   data = request.execute_request_direct(options)
+#   s = json.loads(data.content)
+#   web_title = s['Title']
+#   print("Web title: " + web_title)
+# else:
+#   print(ctx_auth.get_last_error())
