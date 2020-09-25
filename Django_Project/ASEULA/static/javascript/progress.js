ed41dc48784360dac68c3a1658af85c8abe3f4d3
mdpawlow <michaela.pawlowski1998@gmail.com>
2020-09-24 22:01:48 -0700

    Minor CSS changes.

---
+++ Django_Project/ASEULA/static/javascript/progress.js
@@ -0,0 +1,14 @@
+function Show_Progress() {
+    var progressbar = document.getElementById("progress-bar");
+    var disablebutton = document.getElementById("submission-btn");
+    var filebutton = document.getElementById("file-input");
+    var textbutton = document.getElementById("text-input");
+
+    if (progressbar.style.display = "none") {
+        progressbar.style.display = "block";
+    }
+
+    disablebutton.disabled = true;
+    filebutton.disabled = true;
+    textbutton.disabled = true;
+  }
