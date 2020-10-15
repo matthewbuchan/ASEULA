35d5e9deddcbad56609262830ae2d598e41ab842
Matthew Buchan <mbuchan7@asu.edu>
2020-10-15 00:46:43 -0700

    production key removed, new key generated and stored in vault

--- Django_Project/ASEULA/ASEULA/settings.py
+++ Django_Project/ASEULA/ASEULA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p^201)4w=qni!v)#l8&l3srg=*ws3wrzrq$(aadg0wj#*e7(jp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
