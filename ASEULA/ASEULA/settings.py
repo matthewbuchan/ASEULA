f14fc00610d375722d3b24ba1736ce599e4d26ec
Matthew Buchan <mbuchan7@asu.edu>
2020-11-19 14:11:30 -0700

    Choice box selection for sharepoint fixed in powershell script

--- ASEULA/ASEULA/settings.py
+++ ASEULA/ASEULA/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mysecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.100.62', '127.0.0.1','localhost']
+ALLOWED_HOSTS = ['192.168.100.75','localhost']
 
 
 # Application definition
--- export_to_sharepoint.ps1
+++ export_to_sharepoint.ps1
@@ -1,5 +1,4 @@
 ﻿#~ SharePoint Links
-Import-Module WebAdministration
 $listName = 'Capstone EULA Test'
 $rootSite = "https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom"
 #$listSite = "/Lists/Capstone%20EULA%20Test%20List/AllItems.aspx"
@@ -7,26 +6,24 @@ $rootSite = "https://arizonastateu.sharepoint.com/sites/O365FSEETSClassroom"
 # Stores CSV contents to a variable.
 $csv_file = '.\xlsx_dump.csv'
 
-# Stores Restriction Choices for SharePoint checkboxes
-$Choices = @("Instructional-use only","Research-use only","Requires Physical Device","No RDP Use","Use geographically limited (Campus)","Use geographically limited (radius)","US use only","VPN required off-site","Block embargoed countries","Block use from Persons of Concern","On-site (lab) use only","On-site use for on-site students only","Virtualization Not Allowed")
-
 #Add items to SP list (FUNCTIONING)
 
-Function Check-SPListItem([string]$csvInfo){
-    #~ Add New List Item
+Function Check-SPListItem([string]$csvInfo){    
     Connect-PnPOnline -Url $rootSite -UseWebLogin
     $list = Get-PnPList -Identity $listName
     $splist = @{}
     Get-PnPListItem $list -Fields "Id","Title" | ForEach-Object{
-    $splist.Add($_['Title'],$_.Id)
+        $splist.Add($_['Title'],$_.Id)
     }
-    Import-CSV $csvInfo | ForEach-Object{        
-        $choiceitems = $_.'licensing restrictions'.replace(";#",", ")
+    Import-CSV $csv_file | ForEach-Object{    
+        $choiceitems = $_.'licensing restrictions' -split ';#'
+        
+        #~ Update Existing List Item
         if ( $splist.ContainsKey($_.'software name')){
-            $spitem = Set-PnPListItem -List $list -Identity $splist[$_.'software name'] -Values @{"Title"= $_.'software name'; "PublisherName"= $_.'publisher name'; "InformationWebpage"= $_.'information webpage'; "LicensingRestrictions"= $choiceitems}
-            $spitem.update($true)
+            Set-PnPListItem -List $list -Identity $splist[$_.'software name'] -Values @{"Title"= $_.'software name'; "PublisherName"= $_.'publisher name'; "InformationWebpage"= $_.'information webpage'; "LicensingRestrictions"= $choiceitems}
             }
-        else{            
+        #~ Add New List Item
+        else{
             Add-PnPListItem -List $list -Values @{"Title"= $_.'software name'; "PublisherName"= $_.'publisher name'; "InformationWebpage"= $_.'information webpage'; "LicensingRestrictions"= $choiceitems}
             }
         }
